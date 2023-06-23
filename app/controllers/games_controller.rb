353653354eee1d339d9c62e222eae5b01a973b9e
RodCato <code@eliment.org>
2023-06-23 16:56:37 -0500

    Added validations to Game model and tested models and requests for create and index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,33 @@
 class GamesController < ApplicationController
+    def index
+        games = Game.all 
+        render json: games
+    end
+
+    def show 
+        game = Game.find(params[:id])
+        render json: game
+    end
+
+    def create 
+        game = Game.create(game_params)
+        if game.valid?
+            render json: game
+        else
+            render json: game.errors, status: 422
+        end
+    end
+
+    def update
+      
+      end
+
+      def destroy
+
+      end
+
+    private
+    def game_params
+        params.require(:game).permit(:title, :image, :platform, :genre, :notes, :user_id)
+    end
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "Games", type: :request do
+  let(:user) { User.create(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of games' do
+      game = user.games.create(
+        title: 'THE LEGEND OF ZELDA: TEARS OF THE KINGDOM',
+        image: 'https://media.rawg.io/media/resize/420/-/screenshots/c0d/c0dafd15ec182acab8f5879665eaa642.jpg',
+        platform: 'Nintendo Switch',
+        genre: 'Adventure',
+        notes: 'Some good action in this game'
+      )
+      get '/games'
+      game = Game.first
+      expect(response).to have_http_status(200)
+      expect(game['title']).to eq('THE LEGEND OF ZELDA: TEARS OF THE KINGDOM')
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a game with all its attributes" do
+      game_params = {
+        game: {
+          title:'Vectorman',
+          image:'https://upload.wikimedia.org/wikipedia/en/4/43/Vectorman_cover.jpg?20080627132115',
+          platform:'Sega Genesis',
+          genre:'Adventure',
+          notes:'Awesome childhood game!',
+          user_id: user.id
+        }
+      }
+      post '/games', params: game_params
+      game = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      games = Game.all
+      expect(games.length).to be(1)
+    end
   end
 end
