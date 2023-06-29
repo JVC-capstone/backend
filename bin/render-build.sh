7a9946a40a6f51aa997e851c0c919271cca5f8e9
RodCato <code@eliment.org>
2023-06-29 17:02:18 -0500

    update rake file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:seed
 # remove or comment out line 7 after initial build
 # renderbuild
